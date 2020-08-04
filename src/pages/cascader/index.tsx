5596efcb94b00d01d673410a9e5c25f019197eec
DAKingFish <891248330@qq.com>
2020-08-04 21:51:39 +0800

    8.4

--- src/pages/cascader/index.tsx
+++ src/pages/cascader/index.tsx
@@ -4,56 +4,194 @@ import './index.less'; //导入类或文件要加''
  * 级联就是双向绑定,异步操作
  *
  */
+const list1: any = [
+  {
+    value: 1,
+    label: 's1',
+    list: [
+      {
+        value: 1,
+        label: 'Fnc',
+        list: [
+          {
+            value: 1,
+            label: '上单 theshy',
+          },
+          {
+            value: 2,
+            label: '中单 rooike',
+          },
+          {
+            value: 3,
+            label: '打野 ning',
+          },
+          {
+            value: 4,
+            label: 'adc Jacklove',
+          },
+          {
+            value: 5,
+            label: '辅助 baolan',
+          },
+        ],
+      },
+      {
+        value: 2,
+        label: 'TAP',
+        list: [
+          {
+            value: 1,
+            label: '上单 zoom',
+          },
+          {
+            value: 2,
+            label: '中单 yagao',
+          },
+          {
+            value: 3,
+            label: '打野 kanavi',
+          },
+          {
+            value: 4,
+            label: 'adc loken',
+          },
+          {
+            value: 5,
+            label: '辅助 lvmao',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 2,
+    label: 's2',
+    list: [
+      {
+        value: 1,
+        label: 'SKT',
+        list: [
+          {
+            value: 1,
+            label: '上单 zoom',
+          },
+          {
+            value: 2,
+            label: '中单 yagao',
+          },
+          {
+            value: 3,
+            label: '打野 kanavi',
+          },
+          {
+            value: 4,
+            label: 'adc loken',
+          },
+          {
+            value: 5,
+            label: '辅助 lvmao',
+          },
+        ],
+      },
+      {
+        value: 2,
+        label: 'SSW',
+        list: [
+          {
+            value: 1,
+            label: '上单 957',
+          },
+          {
+            value: 2,
+            label: '中单 xiye',
+          },
+          {
+            value: 3,
+            label: '打野 condi',
+          },
+          {
+            value: 4,
+            label: 'adc mystic',
+          },
+          {
+            value: 5,
+            label: '辅助 ben',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 3,
+    label: 's3',
+    list: [
+      {
+        value: 1,
+        label: 'IG',
+        list: [
+          {
+            value: 1,
+            label: '上单 theshy',
+          },
+          {
+            value: 2,
+            label: '中单 rooike',
+          },
+          {
+            value: 3,
+            label: '打野 ning',
+          },
+          {
+            value: 4,
+            label: 'adc Jacklove',
+          },
+          {
+            value: 5,
+            label: '辅助 baolan',
+          },
+        ],
+      },
+      {
+        value: 2,
+        label: 'WE',
+        list: [
+          {
+            value: 1,
+            label: '上单 957',
+          },
+          {
+            value: 2,
+            label: '中单 xiye',
+          },
+          {
+            value: 3,
+            label: '打野 condi',
+          },
+          {
+            value: 4,
+            label: 'adc mystic',
+          },
+          {
+            value: 5,
+            label: '辅助 ben',
+          },
+        ],
+      },
+    ],
+  },
+];
 export default (props: any) => {
-  const [value1, setvalue1] = useState(1);
-  const [value2, setvalue2] = useState();
-  const [list1, setlist1]: any = useState([
-    {
-      value: 1,
-      label: 's1',
-      list: [
-        {
-          value: '1',
-          label: 'Fnc',
-        },
-        {
-          value: '2',
-          label: 'TAP',
-        },
-      ],
-    },
-    {
-      value: 2,
-      label: 's2',
-      list: [
-        {
-          value: '1',
-          label: 'SKT',
-        },
-        {
-          value: '2',
-          label: 'SSW',
-        },
-      ],
-    },
-    {
-      value: 3,
-      label: 's3',
-      list: [
-        {
-          value: '1',
-          label: 'IG',
-        },
-        {
-          value: '2',
-          label: 'WE',
-        },
-      ],
-    },
-  ]);
+  const [value1, setvalue1]: any = useState();
+  const [value2, setvalue2]: any = useState();
+  const [value3, setvalue3]: any = useState();
+  //
   const list2 = list1.find((item: any) => {
     return item.value === value1;
-  });
+  }) || { list: [] };
+  const list3 = list2.list.find((item: any) => {
+    return item.value === value2;
+  }) || { list: [] };
+
   return (
     <div>
       <select
@@ -74,7 +212,7 @@ export default (props: any) => {
       <select
         value={value2}
         onChange={(e: any) => {
-          setvalue2(e.target.value);
+          setvalue2(Number(e.target.value));
         }}
       >
         {//遍历list1中选中value的list属性
@@ -86,9 +224,43 @@ export default (props: any) => {
           );
         })}
       </select>
-      <span>
-        {value1}/{value2}
-      </span>
+
+      <select
+        value={value3}
+        onChange={(e: any) => {
+          setvalue3(e.target.value);
+        }}
+      >
+        {list3.list.map((item: any) => {
+          return (
+            <option key={item.value} value={item.value}>
+              {item.label}
+            </option>
+          );
+        })}
+      </select>
+      <br />
+      <input
+        onChange={(e: any) => {
+          setvalue1(Number(e.target.value));
+        }}
+        placeholder="输入赛季"
+        value={value1}
+      />
+      <input
+        onChange={(e: any) => {
+          setvalue2(Number(e.target.value));
+        }}
+        placeholder="输入队伍"
+        value={value2}
+      />
+      <input
+        onChange={(e: any) => {
+          setvalue3(Number(e.target.value));
+        }}
+        placeholder="位置"
+        value={value3}
+      />
     </div>
   );
 };
